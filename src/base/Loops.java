16027384c01b474ad001dca0ce4f0a8c49ad6294
ClaudiaN1 <nicoricicllaudia@yahoo.ro>
2018-10-28 16:01:29 +0200

    branch created

---
+++ src/base/Loops.java
@@ -0,0 +1,29 @@
+package base;
+
+public class Loops {
+
+    public Loops(){
+        System.out.println("Loops Constructor");
+    }
+
+    public void printSimpleArray () {
+
+
+        int [] integerArray = new int [4];
+
+        integerArray[0]=0;
+        integerArray[1]=1;
+        integerArray[2]=2;
+        integerArray[3]=3;
+
+        for ( int i =0; i < integerArray.length;i++)
+        {
+            System.out.println(integerArray[i]);
+        }
+    }
+
+    public static String message()
+    {
+        return  " Loops ";
+    }
+}
