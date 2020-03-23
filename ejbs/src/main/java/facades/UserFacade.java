2bfcf2218878219565506b0e13ba3328179a9de2
ClaudiaNicorici <44519755+ClaudiaN1@users.noreply.github.com>
2020-03-23 14:56:14 +0200

    Update UserFacade.java

--- ejbs/src/main/java/facades/UserFacade.java
+++ ejbs/src/main/java/facades/UserFacade.java
@@ -7,10 +7,10 @@ import org.hibernate.query.Query;
 public class UserFacade extends BaseFacade {
 
     public User login(String userName, String password) {
-        Session session = openSession();               // am deschis o sesiune
+        Session session = openSession();                
         Query<User> query = session.createQuery("FROM User WHERE userName = :userName",User.class );
-        query.setParameter("userName", userName);            //setam parametru
-        User user = query.getSingleResult();                   //executam query ul
+        query.setParameter("userName", userName);            
+        User user = query.getSingleResult();                    
         if(!user.getPassword().equals(password)){
             user = null;
         }
